09540e25a1b46038c292ee6dbd7605807d1af82a
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-10-29 16:31:38 +0100

    style(main): normalize formatting to comply with 42 norm
    Adjust main.c code style:
    - Reorder variable declarations to top of scope
    - Replace spaces with tabs for indentation
    - Standardize return statement format: return (0)
    - Maintain functional equivalence
    
    No behavioral changes.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lfiorell@student.42nice.fr <lfiorell>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 14:49:38 by lfiorell@st       #+#    #+#             */
-/*   Updated: 2025/10/29 14:55:17 by lfiorell@st      ###   ########.fr       */
+/*   Updated: 2025/10/29 16:27:20 by lfiorell@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char const *argv[])
+int	main(int argc, char const *argv[])
 {
-  if (argc != 2)
-  {
-    printf("Usage: %s <map file>\n", argv[0]);
-    exit(1);
-  }
-  const char *map_file = argv[1];
-  size_t len = strlen(map_file);
-  if (len < 5 || strcmp(map_file + len - 4, ".cub") != 0)
-  {
-    printf("Error: Map file must have .cub extension\n");
-    exit(1);
-  }
-  printf("Starting game with map file: %s\n", argv[1]);
+	const char	*map_file = argv[1];
+	size_t		len;
 
-  return 0;
+	if (argc != 2)
+	{
+		printf("Usage: %s <map file>\n", argv[0]);
+		exit(1);
+	}
+	len = strlen(map_file);
+	if (len < 5 || strcmp(map_file + len - 4, ".cub") != 0)
+	{
+		printf("Error: Map file must have .cub extension\n");
+		exit(1);
+	}
+	printf("Starting game with map file: %s\n", argv[1]);
+	return (0);
 }
