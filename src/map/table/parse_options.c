309bdd5c323124ae9123c4f685a7703db272126b
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-11-03 14:55:00 +0100

    feat(map): implement RGB color parsing for map options
    Implement parsing for RGB color values in map options (Floor and Ceiling).
    
    This change introduces:
    - `is_digit_str`: A helper to check if a string contains only digits.
    - `is_valid_rgb_component`: Validates the format of an RGB component string (e.g., "255,255,255").
    - `parse_rgb_value`: Parses a valid RGB string into an integer color value,
      handling range validation (0-255) and setting `errno` on failure.
    - Updates `setoption` to use `parse_rgb_value` for 'F' and 'C' options.
    - Adds leading whitespace trimming for option values.

--- src/map/table/parse_options.c
+++ src/map/table/parse_options.c
@@ -6,13 +6,15 @@
 /*   By: lfiorell@student.42nice.fr <lfiorell>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 16:38:49 by lfiorell@st       #+#    #+#             */
-/*   Updated: 2025/10/30 17:01:25 by lfiorell@st      ###   ########.fr       */
+/*   Updated: 2025/11/03 14:51:02 by lfiorell@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "map/table.h"
 #include "utils/panic.h"
 #include "utils/string.h"
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -67,9 +69,60 @@ static bool	isvalid_value(const char *value)
 	(void)value;
 	return (true);
 }
+static bool	is_digit_str(const char *s)
+{
+	while (*s)
+	{
+		if (*s < '0' || *s > '9' || *s == ',')
+			return (false);
+		s++;
+	}
+	return (true);
+}
+
+static bool	is_valid_rgb_component(const char *s)
+{
+	int	fr;
+
+	if (!is_digit_str(s))
+		return (false);
+	fr = ft_strfreq(s, ',');
+	if (fr != 2)
+		return (false);
+	return (true);
+}
+
+int	parse_rgb_value(const char *value)
+{
+	char	**limps;
+	int		r;
+	int		g;
+	int		b;
+
+	if (!is_valid_rgb_component(value))
+	{
+		errno = EINVAL;
+		return (-1);
+	}
+	limps = ft_split(value, ',');
+	if (!limps)
+		return (-1);
+	r = ft_atoi(limps[0]);
+	g = ft_atoi(limps[1]);
+	b = ft_atoi(limps[2]);
+	free_string_array((const char **)limps, 3);
+	if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
+	{
+		errno = EINVAL;
+		return (-1);
+	}
+	return ((r << 16) | (g << 8) | b);
+}
 
 static void	setoption(t_map *map, const char *option, const char *value)
 {
+	while (*value == ' ' || *value == '\t')
+		value++;
 	if (strcmp(option, "NO") == 0)
 		map->north_wall_texture = strdup(value);
 	else if (strcmp(option, "SO") == 0)
@@ -79,9 +132,9 @@ static void	setoption(t_map *map, const char *option, const char *value)
 	else if (strcmp(option, "EA") == 0)
 		map->east_wall_texture = strdup(value);
 	else if (strcmp(option, "F") == 0)
-		todo("Parse RGB value", __FILE__, __LINE__);
+		map->floor_color = parse_rgb_value(value);
 	else if (strcmp(option, "C") == 0)
-		todo("Parse RGB value", __FILE__, __LINE__);
+		map->ceiling_color = parse_rgb_value(value);
 }
 
 bool	parse_options(t_map *map, const char **lines, int count)
